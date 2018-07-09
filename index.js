651fb4185cbe2d1841b2f4c7bbf8b611288b203b
FutureScript <fernandobaxter1@gmail.com>
2018-07-09 00:25:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // add solution here
+const musicians = ["John Lennon", "Paul McCArtney", "George Harrison", "Ringo"];
+
+const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (let i = 0; i < 4; i++) {
+      array.push(`${musicians[i]} plays ${instruments[i]}`);
+    }
+  return array;
+}
+
+
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+  function johnLennonFacts(facts) {
+  while () {
+    facts[n] + "!!!";
+  }
+  return facts;  
+}
+
+  
+}
