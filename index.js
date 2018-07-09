7aa4499cfd4c8eaa9f31a9629799d2eed479bc10
FutureScript <fernandobaxter1@gmail.com>
2018-07-09 01:16:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const facts = [
   function johnLennonFacts(facts) {
     let counter = 0
   while (counter < facts.length) {
-    facts + "!!!";
+    facts[i] + "!!!";
     counter++
   }
   return facts;  
